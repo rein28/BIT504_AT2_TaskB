569c88839b2ced5d8e740aa7496a9e9ee64db2be
OEM <OEM@WinYenRei>
2022-09-04 18:20:50 +1200

    changing the Foreground color of GameState when
    Draw = green; Cross_won = Red; Nought_won = Blue

--- TicTacToe/src/GameMain.java
+++ TicTacToe/src/GameMain.java
@@ -116,13 +116,13 @@ public class GameMain extends JPanel implements MouseListener{
 				
 			}       
 			} else if (currentState == GameState.Draw) {          
-				statusBar.setForeground(Color.RED);          
+				statusBar.setForeground(Color.GREEN);          
 				statusBar.setText("It's a Draw! Click to play again.");       
 			} else if (currentState == GameState.Cross_won) {          
 				statusBar.setForeground(Color.RED);          
 				statusBar.setText("'X' Won! Click to play again.");       
 			} else if (currentState == GameState.Nought_won) {          
-				statusBar.setForeground(Color.RED);          
+				statusBar.setForeground(Color.BLUE);          
 				statusBar.setText("'O' Won! Click to play again.");       
 			}
 		}
